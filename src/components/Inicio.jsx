87d31eee6f39a3f70b1c588c6d68f33dab65e918
HatshepsutS <denriquezmont@gmail.com>
2024-05-29 18:32:54 -0600

    Vector del inicio arreglado

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import mainPrueba from '../images/mainPage2.png';
-import logoSP from '../images/SpydyLogo.svg';
+import logoSP from '../images/vectorSpidy.svg';
 export const Inicio = () => {
 
   return (
